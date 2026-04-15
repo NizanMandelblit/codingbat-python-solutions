bd09c2ed8026e5d2bbb0c5f4d38d74840902253a
Nizan Mandelblit <nizanm91@gmail.com>
2026-04-15 12:17:06 +0200

    warmup_1: src

---
+++ src/warmup_1.py
@@ -0,0 +1,137 @@
+def sleep_in(weekday, vacation):
+    """
+    The parameter weekday is True if it is a weekday, and the parameter vacation
+    is True if we are on vacation. We sleep in if it is not a weekday or we're on
+    vacation. Return True if we sleep in.
+    """
+    if not weekday or vacation:
+        return True
+    return False
+
+
+def monkey_trouble(a_smile, b_smile):
+    """
+    We have two monkeys, a and b, and the parameters a_smile and b_smile indicate
+    if each is smiling. We are in trouble if they are both smiling or if neither
+    of them is smiling. Return True if we are in trouble.
+    """
+    if a_smile and b_smile:
+        return True
+    elif not a_smile and not b_smile:
+        return True
+    return False
+
+
+def sum_double(a, b):
+    """
+    Given two int values, return their sum. Unless the two values are the same,
+    then return double their sum.
+    """
+    res = a+b
+    if a == b:
+        return res*2
+    return res
+
+
+def diff21(n):
+    """
+    Given an int n, return the absolute difference between n and 21, except return
+    double the absolute difference if n is over 21.
+    """
+    diff = abs(21 - n)
+    if n > 21:
+        return diff*2
+    return diff
+
+
+def parrot_trouble(talking, hour):
+    """
+    We have a loud talking parrot. The "hour" parameter is the current hour time
+    in the range 0..23. We are in trouble if the parrot is talking and the hour
+    is before 7 or after 20. Return True if we are in trouble.
+    """
+    if talking and (hour < 7 or hour > 20):
+        return True
+    return False
+
+
+def makes10(a, b):
+    """
+    Given 2 ints, a and b, return True if one if them is 10 or if their sum is 10.
+    """
+    return ((a+b == 10) or a == 10 or b == 10)
+
+
+def near_hundred(n):
+    """
+    Given an int n, return True if it is within 10 of 100 or 200.
+    """
+    hundred = abs(n-100)
+    two_hundred = abs(n-200)
+    return (hundred <= 10) or (two_hundred <= 10)
+
+
+def pos_neg(a, b, negative):
+    """
+    Given 2 int values, return True if one is negative and one is positive.
+    Unless the parameter "negative" is True, then they both must be negative.
+    """
+    if negative:
+        return a < 0 and b < 0
+    return (a*b < 0)
+
+
+def not_string(str):
+    """
+    Given a string, return a new string where "not " has been added to the front.
+    However, if the string already begins with "not", return the string unchanged.
+    """
+    if "not" in str[:3]:  # also possible if str.startswith("not")
+        return str
+    return "not "+str
+
+
+def missing_char(str, n):
+    """
+    Given a non-empty string and an int n, return a new string where the char
+    at index n has been removed. The value of n will be a valid index of a char in
+    the original string (i.e. n will be in the range 0..len(str)-1 inclusive).
+    """
+    i = 0
+    res = ""
+    for c in str:
+        if i != n:
+            res += c
+        i = i + 1
+    return res
+    # front = str[:n]   # up to but not including n
+    # back = str[n+1:]  # n+1 through end of string
+    # return front + back
+
+
+def front_back(str):
+    """
+    Given a string, return a new string where the first and last chars
+    have been exchanged.
+    """
+    if len(str) <= 1:
+        return str
+
+    mid = str[1:-1]  # Everything except the first and last characters
+
+    # Swap: Last + Middle + First
+    return str[-1] + mid + str[0]
+
+
+def front3(str):
+    """
+    Given a string, we'll say that the front is the first 3 chars of the string.
+    If the string length is less than 3, the front is whatever is there.
+    Return a new string which is 3 copies of the front.
+    """
+    if len(str) < 3:
+        front = str
+    else:
+        front = str[0:3]
+    front *= 3
+    return front
